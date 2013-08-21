301b5c44c76d521c17870010936996adea453e97
skovsgaard <bastian.1343@gmail.com>
2013-08-21 02:21:39 +0200

    First commit, local

---
+++ index.js
@@ -0,0 +1,3 @@
+var podcatcher = require('./podcatcher');
+
+podcatcher('http://podcast.dr.dk/p3/rssfeed/monte_carlo.xml', 'out');
---
+++ podcatcher.js
@@ -0,0 +1,20 @@
+var feedparser = require('ortoo-feedparser');
+var request = require('request');
+var fs      = require('fs');
+
+//function writeToFile(input, filename) {
+//  var writer = fs.createWriteStream(filename);
+//  writer.write(input);
+//}
+
+function podcatcher(input, output) {
+  var getMedia = function(err, meta, articles) {
+    var feedMedia = articles[0].enclosures[0].url;
+    console.log('Downloading...');
+    request(feedMedia).pipe(fs.createWriteStream('out.mp3');
+  };
+
+  feedparser.parseUrl(input, getMedia);
+}
+
+module.exports = podcatcher;
