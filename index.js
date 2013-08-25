fd28ebfdf11d334a24b07fd3298f844f187748c6
skovsgaard <bastian.1343@gmail.com>
2013-08-25 20:18:28 +0200

    Updated calbacks and error handling.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var podcatcher = require('./podcatcher');
 
-podcatcher('http://podcast.dr.dk/p3/rssfeed/monte_carlo.xml', 'out');
+var callback = function(err, meta) {
+  if (err) console.log(err);
+  if (meta) console.log('\n' + meta.description + '\n');
+}
+
+podcatcher.getByDate('http://feeds.feedburner.com/omgcraftsd', '2013-08-03', callback);
--- podcatcher.js
+++ podcatcher.js
@@ -2,21 +2,98 @@ var feedparser = require('ortoo-feedparser');
 var request = require('request');
 var fs      = require('fs');
 
-//function writeToFile(input, filename) {
-//  var writer = fs.createWriteStream(filename);
-//  writer.write(input);
-//}
+// Utility function to download media at specified URL
+function downloadMedia(mediaUrl, title) {
+  var media = request(mediaUrl).pipe(fs.createWriteStream(title));
 
-function podcatcher(input, output) {
-  var getMedia = function(err, meta, articles) {
-    var feedMedia = articles[0].enclosures[0].url;
-    console.log('Downloading...');
-    request(feedMedia).pipe(fs.createWriteStream('out.mp3').on('finish', function() {
-      console.log('Done!');
-    }));
-  };
+  console.log('Downloading ' + title);
 
-  feedparser.parseUrl(input, getMedia);
+  var waitTick = setInterval(function() {
+    console.log("...");
+  }, 20000);
+
+  media.on('finish', function() {
+    clearInterval(waitTick);
+    console.log(title + ' is done!');
+  });
+}
+
+function getLatest(meta, articles) {
+    var latest = articles[0];
+    var latestMedia = latest.enclosures[0].url;
+    var latestDate = latest.pubDate.toISOString().slice(0,10);
+    var extension = latest.enclosures[0].type;
+    extension = '.' + extension.slice(extension.indexOf('/'), extension.length);
+    var episodeTitle = meta.title + '-' + latestDate + extension;
+    episodeTitle = episodeTitle.replace(/\s+/g, '').toLowerCase();
+
+    downloadMedia(latestMedia, episodeTitle);
+}
+
+function getByDate(date, meta, articles) {
+    var article;
+    var media;
+    var title;
+    for (var i=0; i< articles.length; i++) {
+      if (articles[i].pubDate.toISOString().slice(0,10) == date) {
+        article = articles[i];
+      }
+    }
+    media = article.enclosures[0].url;
+    var extension = article.enclosures[0].type;
+    extension = '.' + extension.slice(extension.indexOf('/')+1, extension.length);
+    title = meta.title + '-' + date + extension;
+    title = title.replace(/\s+/g, '').toLowerCase();
+
+    downloadMedia(media, title);
+}
+
+// Get the latest media in the specified podcast stream and download it.
+podcatcher.getNewest = function(feedUrl, cb) {
+  var err;
+
+  // Callback to locate the newest media and download it
+  function getArticle(err, meta, articles) {
+    getLatest(meta, articles);
+
+    if (meta) {
+      cb(null, meta);
+    } else {
+      cb(new Error('Error 500'));
+    }
+  }
+
+  var newestMedia = feedparser.parseUrl(feedUrl, getArticle);
+};
+
+// Get the specified article in the specified podcast stream and download it.
+podcatcher.getByDate = function(feedUrl, date, cb) {
+
+  // Callback to locate the media at date and download it
+  function getArticle(err, meta, articles) {
+    getByDate(date, meta, articles);
+
+    if (meta) {
+      cb(null, meta);
+    } else if (err) {
+      cb(new Error('Error 500'));
+    }
+  }
+
+  // Parse feed and download in parser's callback
+  var media = feedparser.parseUrl(feedUrl, getArticle);
+
+};
+
+// Default function for module, returning metadata of specified feed.
+function podcatcher(feedUrl, cb) {
+  feedparser.parseUrl(feedUrl, function(err, meta, articles) {
+    if (err) {
+      cb(err, meta);
+    } else {
+      cb(null, meta);
+    }
+  });
 }
 
 module.exports = podcatcher;
